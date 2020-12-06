8249c2e39d57603a4862a733d8a7c98a1bb4884a
Marcin Baranek <55330253+MarcinBaranek@users.noreply.github.com>
2020-12-06 17:08:28 +0100

    Create eig_method_power.py

---
+++ eig_method_power.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+
+def eig_value_power_method(matrix: np.array, init_vector=None, max_iter=None, epsilon=None):
+    if max_iter is None:
+        max_iter = 5 * matrix.shape[0]
+    if init_vector is None:
+        init_vector = np.ones(shape=(matrix.shape[0],))
+
+    for i in range(max_iter):
+        init_vector = np.dot(matrix, init_vector) / np.sqrt(np.sum(init_vector ** 2))
+
+    index = np.unravel_index(np.argmax(np.dot(matrix, init_vector), axis=None), init_vector.shape)
+    return np.dot(matrix, init_vector)[index] / init_vector[index], init_vector / np.sqrt(np.sum(init_vector ** 2))
